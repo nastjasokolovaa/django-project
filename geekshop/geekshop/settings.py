55a33af5539ad1e02c52611f1d88ead528d277dd
nastjas <nastjasokolovaa@gmail.com>
2021-05-03 22:06:52 +0300

    lesson-2: work with templates and context

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'geekshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['geekshop/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,7 +121,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "geekshop", "static"),
+)
+
+# для поиска папок со статикой внутри папок приложений
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 
 # Default primary key field type
