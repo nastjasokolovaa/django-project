a6c9da8e04745a72ef6d12c73167f7c8d529dfbc
nastjas <nastjasokolovaa@gmail.com>
2021-07-08 22:15:38 +0300

    work with js

--- geekshop/basketapp/models.py
+++ geekshop/basketapp/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 import geekshop
 from mainapp.models import Product
+from ordersapp.models import Order
+
+
+# class BasketQuerySet(models.QuerySet):
+#
+#     def delete(self):
+#         for item in self:
+#             item.product.quantity += item.quantity
+#             item.product.save()
+#         super().delete()
 
 
 class Basket(models.Model):
+    # objects = BasketQuerySet.as_manager()
+
     user = models.ForeignKey(
         geekshop.settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
@@ -40,3 +52,19 @@ class Basket(models.Model):
         _items = Basket.objects.filter(user=self.user)
         _total_cost = sum(list(map(lambda x: x.product_cost, _items)))
         return _total_cost
+
+    @staticmethod
+    def get_item(pk):
+        return Basket.objects.get(pk=pk)
+
+    # def delete(self, *args, **kwargs):
+    #     self.product.quantity += self.quantity
+    #     self.product.save()
+    #     super().delete()
+    #
+    # def save(self, *args, **kwargs):
+    #     if self.pk:
+    #         self.product.quantity -= self.quantity - self.__class__.objects.get(pk=self.pk).quantity
+    #     else:
+    #         self.product.quantity -= self.quantity
+    #     super(Basket, self).save(*args, **kwargs)
