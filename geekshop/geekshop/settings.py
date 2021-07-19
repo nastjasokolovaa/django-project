3f26f83f9fa61db8f8fd203caec87b36cfde0635
nastjas <nastjasokolovaa@gmail.com>
2021-07-19 19:41:35 +0300

    changed settings

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'social_django.middleware.SocialAuthExceptionMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 if DEBUG:
